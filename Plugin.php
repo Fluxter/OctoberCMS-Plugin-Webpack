398850f61019cbbc5fd0e22f5ff8719eaf373f81
Marcel Kallen <root@4b6e7533c8c6>
2020-10-04 20:48:39 +0000

    added functions

--- Plugin.php
+++ Plugin.php
@@ -16,15 +16,27 @@ class Plugin extends PluginBase
         ];
     }
 
+    private function getEntrypoints(string $theme)
+    {
+        return json_decode(file_get_contents(__DIR__ . "/../../../themes/" . $theme . "/assets/build/entrypoints.json"), true);
+    }
+
     public function webpackStyles(string $entrypoint)
     {
-        // <link rel="stylesheet" type="text/css" href="{{ 'assets/build/app.css'|theme }}" />
-        return "hello world styles." . $entrypoint;
+        $ep = $this->getEntrypoints("theme-fx2020");
+        foreach ($ep["entrypoints"][$entrypoint]["css"] as $path) {
+            $html = '<link rel="stylesheet" type="text/css" href="' . $path . '" />';
+        }
+        return $html;
     }
 
     public function webpackScripts(string $entrypoint)
     {
-        // <link rel="stylesheet" type="text/css" href="{{ 'assets/build/app.css'|theme }}" />
-        return "hello world";
+        $ep = $this->getEntrypoints("theme-fx2020");
+        $html = "";
+        foreach ($ep["entrypoints"][$entrypoint]["js"] as $path) {
+            $html = '<script src="' . $path . '" />';
+        }
+        return $html;
     }
 }
